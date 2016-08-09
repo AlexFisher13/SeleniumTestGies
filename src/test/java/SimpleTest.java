16a734d1a56a4e1ab9ca8788d5615db2351454d9
user <alexfisher13@yandex.ru>
2016-08-09 14:12:22 +0300

    `12`

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
@@ -11,24 +12,24 @@ import static com.thoughtworks.selenium.SeleneseTestNgHelper.assertEquals;
  */
 public class SimpleTest {
 
-        WebDriver driver;
-        String baseUrl;
+    WebDriver driver;
+    String baseUrl;
 
     @BeforeClass
-    public void before() {
+    public void loginToGies() {
         System.setProperty("webdriver.chrome.driver", "/home/user/chromeDriver/chromedriver");
         driver = new ChromeDriver();
         baseUrl = "http://demo.gies.ivt.lan";
-    }
-
-    @Test
-    public void loginToGies() {
         driver.manage().window().maximize();
         driver.get(baseUrl + "/login");
         driver.findElement(By.cssSelector("[name=username]")).clear();
         driver.findElement(By.cssSelector("[name=username]")).sendKeys("Tkachenko");
         driver.findElement(By.cssSelector("[name=password]")).sendKeys("qwerty");
         driver.findElement(By.cssSelector("[class=login-button]")).click();
+    }
+
+    @Test
+    public void pageTitle() {
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         System.out.println("Page title is: " + driver.getTitle());
         assertEquals("Gloria", driver.getTitle());
@@ -36,9 +37,28 @@ public class SimpleTest {
 
     @Test
     public void firstPage() {
-        //loginToGies();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         assertEquals("Выберите объект энергосервиса", driver.findElement(By.id("tbtext-1448")));
     }
 
+    @Test
+    public void potrebleniePage() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.id("ext-element-16")).click();
+    }
 
+
+
+
+
+
+
+
+
+
+
+    @AfterClass
+    public void finish() {
+        driver.quit();
+    }
 }
